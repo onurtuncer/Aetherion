4c9b197ed1807093542a33cd539cee2e7111b9ff
Onur Tuncer <onur.tuncer@milteksan.com>
2025-11-24 19:23:36 +0300

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// ------------------------------------------------------------------------------
+// Project: Aetherion
+// Copyright(c) 2025, Onur Tuncer, PhD, Istanbul Technical University
+//
+// SPDX - License - Identifier: MIT
+// License - Filename: LICENSE
+// ------------------------------------------------------------------------------
+
+int main() {
+	return 0;
+}
