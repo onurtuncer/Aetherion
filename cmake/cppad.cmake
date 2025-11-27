1d90d384b4f4e041042ca7a767004a6c48606321
Onur Tuncer <onur.tuncer@milteksan.com>
2025-11-28 01:07:40 +0300

    added built cppad library and headers into vendor.

--- cmake/cppad.cmake
+++
@@ -1,7 +0,0 @@
-# cppad.cmake
-add_library(CppAD::cppad INTERFACE IMPORTED)
-
-set_target_properties(CppAD::cppad PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES
-        "${CMAKE_CURRENT_SOURCE_DIR}/vendor/cppad/include"
-)
