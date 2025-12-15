85ebbdca141b8c3544b87f05093f56bf2d14634a
Onur Tuncer <onur.tuncer@milteksan.com>
2025-12-16 00:20:48 +0300

    TODO items

---
+++ Aetherion/Aerodynamics/MachNumber.h
@@ -0,0 +1,9 @@
+// ------------------------------------------------------------------------------
+// Project: Aetherion
+// Copyright(c) 2025, Onur Tuncer, PhD, Istanbul Technical University
+//
+// SPDX - License - Identifier: MIT
+// License - Filename: LICENSE
+// ------------------------------------------------------------------------------
+
+#pragma once
