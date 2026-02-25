be57ff61433bb34b52ff4a255ee90353b5552247
Onur Tuncer <onur.tuncer@milteksan.com>
2026-02-25 12:22:34 +0300

    modified ODEContextBase.h

--- Aetherion/FlightDynamics/ODEContextBase.h
+++ Aetherion/FlightDynamics/ODEContextBase.h
@@ -12,24 +12,24 @@
 #include "Aetherion/FlightDynamics/InertialParameters.h"
 
 #include "Aetherion/Spatial/Inertia.h"
+#include "Aetherion/Spatial/Wrench.h"
 
 namespace Aetherion::FlightDynamics {
 
 	struct ODEContextBase {
 
 		Spatial::Inertia M;
-		double m; // [kg] Mass
-		Eigen::Matrix3d I_com; // [kg·m^2] Inertia w.r.t center-of-mass (CoM)
-		Eigen::Vector3d c; // [m] Kütle merkezi ofseti
-		Eigen::Vector3d tau; // [N·m] Body torque (control)  //TODO this needs to be function pointer!
-		Eigen::Vector3d force; // [N] Body force         //TODO this needs to be a function pointer!
+		
 		double mdot; // [kg/s] Mass flow rate (due to fuel burn) //TODO this needs to be a function pointer
-		// ... other parameters ...
 
-		ODEContextBase(Inertial) {
+		ODEContextBase(InertialParameters ip) {
 
 		}
+
+		Spatial::Wrench CalculateAerodynamicWrench() = 0;
+		Spatial::Wrench CalculateGravitationalWrench() = 0;
+		Spatial::Wrench CalculatePropulsiveWrench() = 0;
 	};
 
-}
+} // namespace Aetherion::FlignhtDynamics
 
