70573b9bbbb9a47c990880e175a35dc05cfb9c48
Onur Tuncer <onur.tuncer@milteksan.com>
2026-03-09 14:25:12 +0300

    added placeholder files for prospective Estimation related work

---
+++ Aetherion/Estimation/EKF.h
@@ -0,0 +1,16 @@
+// ------------------------------------------------------------------------------
+// Project: Aetherion
+// Copyright(c) 2025-2026, Onur Tuncer, PhD, Istanbul Technical University
+//
+// SPDX - License - Identifier: MIT
+// License - Filename: LICENSE
+// ------------------------------------------------------------------------------
+
+#pragma once
+
+namespace Aetherion::Estimation {
+
+	//TODO this is a placeholder for time being
+
+
+} // namespace Aetherion::Estimation
