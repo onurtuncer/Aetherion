1a61ef1c251c12ee9251fbf3d3c60ad333c8d394
Onur Tuncer <onur.tuncer@milteksan.com>
2026-03-12 18:12:54 +0300

    comment out unused sections for later removal

--- Aetherion/RigidBody/IWrenchProvider.h
+++ Aetherion/RigidBody/IWrenchProvider.h
@@ -1,40 +1,40 @@
-// ------------------------------------------------------------------------------
-// Project: Aetherion
-// Copyright(c) 2025-2026, Onur Tuncer, PhD,
-// Istanbul Technical University
+//// ------------------------------------------------------------------------------
+//// Project: Aetherion
+//// Copyright(c) 2025-2026, Onur Tuncer, PhD,
+//// Istanbul Technical University
+////
+//// SPDX-License-Identifier: MIT
+//// License-Filename: LICENSE
+//// ------------------------------------------------------------------------------
 //
-// SPDX-License-Identifier: MIT
-// License-Filename: LICENSE
-// ------------------------------------------------------------------------------
-
-#pragma once
-
-#include "Aetherion/Spatial/Wrench.h"
-
-namespace Aetherion::RigidBody {
-
-    template<class Scalar>
-    struct WrenchContext
-    {
-        // Inertial position from Earth center [m], expressed in W
-        Aetherion::Environment::Vec3<Scalar> r_W;
-
-        // Rotation mapping W vectors into body frame B: x_B = R_BW * x_W
-        Eigen::Matrix<Scalar, 3, 3> R_BW;
-
-        // Mass [kg]
-        Scalar mass_kg;
-
-    };
-
-    template<class Scalar>
-    struct IWrenchProvider
-    {
-        virtual ~IWrenchProvider() = default;
-
-        // Canonical output: Wrench expressed in body frame at CG
-        virtual Aetherion::Spatial::Wrench<Scalar>
-            ComputeWrench_B_at_CG(const WrenchContext<Scalar>& ctx) const = 0;
-    };
-
-} // namespace Aetherion::RigidBody
+//#pragma once
+//
+//#include "Aetherion/Spatial/Wrench.h"
+//
+//namespace Aetherion::RigidBody {
+//
+//    template<class Scalar>
+//    struct WrenchContext
+//    {
+//        // Inertial position from Earth center [m], expressed in W
+//        Aetherion::Environment::Vec3<Scalar> r_W;
+//
+//        // Rotation mapping W vectors into body frame B: x_B = R_BW * x_W
+//        Eigen::Matrix<Scalar, 3, 3> R_BW;
+//
+//        // Mass [kg]
+//        Scalar mass_kg;
+//
+//    };
+//
+//    template<class Scalar>
+//    struct IWrenchProvider
+//    {
+//        virtual ~IWrenchProvider() = default;
+//
+//        // Canonical output: Wrench expressed in body frame at CG
+//        virtual Aetherion::Spatial::Wrench<Scalar>
+//            ComputeWrench_B_at_CG(const WrenchContext<Scalar>& ctx) const = 0;
+//    };
+//
+//} // namespace Aetherion::RigidBody
--- Aetherion/RigidBody/SumWrenches.h
+++ Aetherion/RigidBody/SumWrenches.h
@@ -1,18 +1,18 @@
-
-
-// Aetherion/RigidBody/SumWrenches.h
-#pragma once
-
-#include <memory>
-#include <vector>
-
-#include "Aetherion/Spatial/WrenchOps.h"
-#include "Aetherion/RigidBody/IWrenchProvider.h"
-
-namespace Aetherion::RigidBody {
-
-    template<class Scalar>
-    inline Aetherion::Spatial::Wrench<Scalar>
-        SumWrenches_B_at_CG(
-            const WrenchContext<Scalar>& ctx,
-            const std::vector<std::unique_ptr<IWrenchProvider<Scalar>>>& providers)
+//
+//
+//// Aetherion/RigidBody/SumWrenches.h
+//#pragma once
+//
+//#include <memory>
+//#include <vector>
+//
+//#include "Aetherion/Spatial/WrenchOps.h"
+//#include "Aetherion/RigidBody/IWrenchProvider.h"
+//
+//namespace Aetherion::RigidBody {
+//
+//    template<class Scalar>
+//    inline Aetherion::Spatial::Wrench<Scalar>
+//        SumWrenches_B_at_CG(
+//            const WrenchContext<Scalar>& ctx,
+//            const std::vector<std::unique_ptr<IWrenchProvider<Scalar>>>& providers)
