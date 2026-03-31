837a03e7fe79290d695a0ab098a278fdd502574e
Onur Tuncer <otuncer1@gmail.com>
2026-03-31 10:48:37 +0300

    removed duplication of wgs84 constants

--- Aetherion/Environment/Gravity.h
+++ Aetherion/Environment/Gravity.h
@@ -2,8 +2,8 @@
 // Project: Aetherion
 // Copyright(c) 2025-2026, Onur Tuncer, PhD, Istanbul Technical University
 //
-// SPDX - License - Identifier: MIT
-// License - Filename: LICENSE
+// SPDX-License-Identifier: MIT
+// License-Filename: LICENSE
 // ------------------------------------------------------------------------------
 
 #pragma once
@@ -12,26 +12,28 @@
 #include <cstddef>
 #include <cmath>
 
-#include <Aetherion/Environment/detail/MathWrappers.h> 
+#include <Aetherion/Environment/WGS84.h>
+#include <Aetherion/Environment/detail/MathWrappers.h>   // detail::SquareRoot
 
 namespace Aetherion::Environment {
 
     template <class Scalar>
     using Vec3 = std::array<Scalar, 3>;
 
-    /// Central Newtonian gravity (point-mass Earth) in an inertial/ECI frame.
-    ///
-    /// Input:
-    ///   r_W  : position [m] of body in ECI/inertial frame W, from Earth's center.
-    /// Constants:
-    ///   mu   : Earth's GM [m^3/s^2] (default: WGS-84 value)
-    ///
-    /// Output:
-    ///   g_W  : gravitational acceleration [m/s^2] in frame W.
+    // -------------------------------------------------------------------------
+    // CentralGravity
+    //
+    // Point-mass (Newtonian) gravitational acceleration in ECI/inertial frame.
+    //
+    // Input:  r_W   -- position [m] from Earth centre, expressed in ECI
+    // Output: g_W   -- gravitational acceleration [m/s^2] in ECI
+    //
+    // Default mu = WGS-84 geocentric gravitational parameter.
+    // -------------------------------------------------------------------------
     template <class Scalar>
     inline Vec3<Scalar> CentralGravity(
         const Vec3<Scalar>& r_W,
-        const Scalar& mu = Scalar(3.986004418e14)) // [m^3/s^2]
+        const Scalar& mu = Scalar(WGS84::kGM_m3_s2))
     {
         using detail::SquareRoot;
 
@@ -41,63 +43,58 @@ namespace Aetherion::Environment {
 
         const Scalar r2 = x * x + y * y + z * z;
         const Scalar r = SquareRoot(r2);
-        const Scalar r3 = r2 * r;         // r^3
-        const Scalar inv_r3 = Scalar(1) / r3;
+        const Scalar inv_r3 = Scalar(1) / (r2 * r);
 
         const Scalar k = -mu * inv_r3;
 
-        return Vec3<Scalar>{k* x, k* y, k* z};
+        return Vec3<Scalar>{ k* x, k* y, k* z };
     }
 
-    /// Central gravity *with J2* perturbation.
-    ///
-    /// Input:
-    ///   r_W  : position [m] in ECI/inertial frame W.
-    ///   mu   : GM [m^3/s^2]
-    ///   Re   : mean equatorial radius [m]
-    ///   J2   : 2nd zonal harmonic [-]
-    ///
-    /// Output:
-    ///   g_W  : acceleration [m/s^2] including J2.
+    // -------------------------------------------------------------------------
+    // J2
+    //
+    // Gravitational acceleration including the WGS-84 J2 oblateness perturbation.
+    // Calls CentralGravity() for the central term and adds the J2 delta.
+    //
+    // Input:  r_W      -- position [m] in ECI
+    //         mu       -- GM  [m^3/s^2]      (default: WGS-84)
+    //         Re       -- equatorial radius [m]  (default: WGS-84 semi-major axis)
+    //         J2_coeff -- second zonal harmonic [-]  (default: WGS-84)
+    //
+    // Output: g_W -- total acceleration [m/s^2] (central + J2 perturbation)
+    // -------------------------------------------------------------------------
     template <class Scalar>
     inline Vec3<Scalar> J2(
         const Vec3<Scalar>& r_W,
-        const Scalar& mu = Scalar(3.986004418e14),
-        const Scalar& Re = Scalar(6378137.0),
-        const Scalar& J2 = Scalar(1.08262668e-3))
+        const Scalar& mu = Scalar(WGS84::kGM_m3_s2),
+        const Scalar& Re = Scalar(WGS84::kSemiMajorAxis_m),
+        const Scalar& J2_coeff = Scalar(WGS84::kJ2))
     {
         using detail::SquareRoot;
 
+        // Central term -- no code duplication
+        Vec3<Scalar> g = CentralGravity(r_W, mu);
+
+        // J2 perturbation delta
         const Scalar x = r_W[0];
         const Scalar y = r_W[1];
         const Scalar z = r_W[2];
 
         const Scalar r2 = x * x + y * y + z * z;
         const Scalar r = SquareRoot(r2);
-        const Scalar r3 = r2 * r;
-        const Scalar r5 = r3 * r2;
-        const Scalar inv_r3 = Scalar(1) / r3;
+        const Scalar r5 = r2 * r2 * r;
         const Scalar inv_r5 = Scalar(1) / r5;
 
-        // --- Central term ---
-        const Scalar k_c = -mu * inv_r3;
-        Scalar gx = k_c * x;
-        Scalar gy = k_c * y;
-        Scalar gz = k_c * z;
-
-        // --- J2 perturbation ---
-        const Scalar z2 = z * z;
         const Scalar Re2 = Re * Re;
-        const Scalar k_J2 = Scalar(1.5) * J2 * mu * Re2 * inv_r5;
+        const Scalar k_J2 = Scalar(1.5) * J2_coeff * mu * Re2 * inv_r5;
 
-        const Scalar factor_xy = Scalar(5) * z2 / r2 - Scalar(1);
-        const Scalar factor_z = Scalar(5) * z2 / r2 - Scalar(3);
+        const Scalar five_z2_over_r2 = Scalar(5) * z * z / r2;
 
-        gx += k_J2 * factor_xy * x;
-        gy += k_J2 * factor_xy * y;
-        gz += k_J2 * factor_z * z;
+        g[0] += k_J2 * (five_z2_over_r2 - Scalar(1)) * x;
+        g[1] += k_J2 * (five_z2_over_r2 - Scalar(1)) * y;
+        g[2] += k_J2 * (five_z2_over_r2 - Scalar(3)) * z;
 
-        return Vec3<Scalar>{gx, gy, gz};
+        return g;
     }
 
 } // namespace Aetherion::Environment
